21525b55f0643b99e6554678337abbf23bae9efb
princypatel-creator <patelprincy9605@gmail.com>
2026-07-08 12:16:15 +0530

    save

--- gate-pass.js
+++ gate-pass.js
@@ -1,5 +1,5 @@
 function f1(){
-
+// define variable to take the input from the respective fields--
     const pname = document.getElementById('pname').value;
     const phone = document.getElementById('phone').value;
     const fathername = document.getElementById('fathername').value;
@@ -12,6 +12,11 @@ function f1(){
     const branch = document.getElementById('branch').value;
     const age = document.getElementById('age').value;
     const gender = document.getElementById('gender').value;
+
+
+
+//-- save the data in local storage thorugh the key value pair--
+
 
     localStorage.setItem('Pname', pname);
     localStorage.setItem('Phone', phone);
@@ -26,10 +31,16 @@ function f1(){
     localStorage.setItem('Age', age);
     localStorage.setItem('Gender', gender);
 
+
+    //--- a alert msg display after submiting the data of the form--
     alert('Data saved successfully!');
 }
 
 function f2(){
+    //-alert msg for deleting the data from local storage--
+     alert("Are you sure you want to delete the data?");
+
+      //-- delete the data from local storage---
      localStorage.clear();
-    alert("Are you sure you want to delete the data?");
+   
 }
